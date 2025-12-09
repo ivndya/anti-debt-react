1eb6238f46528000b1b368007e27506ab22c0e8e
ivndya <ivn.dya@gmail.com>
2025-12-09 20:20:09 +0500

    fix: remove white border

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import AntiDebtApp from './App'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div
       style={{
+        margin: 0,
+        padding: 0,
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
