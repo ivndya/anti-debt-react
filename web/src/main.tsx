a72e3a77a0b94a24705f2a75ea7b6f5c4b19e6d8
ivndya <ivn.dya@gmail.com>
2025-12-11 22:57:50 +0500

    chore: добавлены типы для импорта CSS

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,7 +4,7 @@ import AntiDebtApp from './App'
 import { FinanceProvider } from './shared/finance-context/FinanceProvider'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <FinanceProvider>
       <div className="m-0 p-0 flex justify-center items-center min-h-screen bg-black">
